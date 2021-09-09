d31c8c3a22e4201e75f10b7017864b8b3951e3f1
RRRRRRRRRR111111111 <90401400+RRRRRRRRRR111111111@users.noreply.github.com>
2021-09-09 16:54:21 +0000

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -1,4 +1,5 @@
-
+#coding=utf-8
+import pdfkit
 import smtplib
 import os
 from email.mime.multipart import MIMEMultipart
@@ -7,45 +8,5 @@ from email.mime.base import MIMEBase
 from email.mime.application import MIMEApplication
 from email import encoders
 
-file_path = r'C:\Users\khaled\Downloads\MFLEET.pdf'
-print( os.path.isfile(file_path))
-
-
-#for f in os.listdir("/home/khaled/python_error/docs/"):
-	#print(f)
-
-fromaddr = "rnoo.27soso@gmail.com"
-toaddr = "rania.m.abdelkerim@gmail.com"
-
-msg = MIMEMultipart()
-
-msg['From'] = fromaddr
-msg['To'] = toaddr
-msg['Subject'] = "subject"
-
-body = ""
-
-msg.attach(MIMEText(body, 'plain'))
-
-
-#file_path = r'C:\Users\khaled\Downloads\MFLEET.pdf'
-#part = MIMEBase('application', 'octet-stream')
-#part.set_payload((attachment).read())
-#encoders.encode_base64(part)
-#part.add_header('Content-Disposition', "attachment; filename= %s" % file_path)
-
-#msg.attach(part)
-filename = 'MFLEET.pdf' #path t
-fo=open(filename,'rb')
-attach = MIMEApplication(fo.read(),_subtype="pdf")
-fo.close()
-attach.add_header('Content-Disposition','attachment',filename=filename)
-msg.attach(attach)
-
-
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(fromaddr, "rbppzxtvzrwsvicu")
-text = msg.as_string()
-server.sendmail(fromaddr, toaddr, text)
-server.quit()
+config = pdfkit.configuration(wkhtmltopdf = "C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe")
+pdfkit.from_url('https://tets.com/index.php?module=index.php', 'out.pdf',configuration=config)
